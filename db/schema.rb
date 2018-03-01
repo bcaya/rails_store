44bfbcdf26421b5c194f588345295ff26fce8a51
Bobby Caya <bob.caya1@gmail.com>
2018-03-01 09:46:38 -0700

    Adds comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228212128) do
+ActiveRecord::Schema.define(version: 20180301162708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.string "author"
+    t.bigint "items_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["items_id"], name: "index_comments_on_items_id"
+  end
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
@@ -31,5 +40,6 @@ ActiveRecord::Schema.define(version: 20180228212128) do
     t.index ["department_id"], name: "index_items_on_department_id"
   end
 
+  add_foreign_key "comments", "items", column: "items_id"
   add_foreign_key "items", "departments"
 end
